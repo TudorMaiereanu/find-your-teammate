6e9eda98bdc24dd2f980dac4936948dd2d3f5cf2
Tudor Maiereanu <tudormaiereanu@gmail.com>
2020-07-13 18:20:13 +0200

    push Leons changes

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -51,10 +51,10 @@ const SearchBox = () => {
                     onChange={setSelected}
                     labelledBy={"Select"}
                     overrideStrings={{
-                        "selectSomeItems": "Search for quality",
-                        "allItemsAreSelected": "All qualities are selected.",
-                        "selectAll": "I want all of them!",
-                        "search": "Search for what you're looking for"
+                        "selectSomeItems": "Search character trait",
+                        "allItemsAreSelected": "All character traits selected.",
+                        "selectAll": "I want all of them.",
+                        "search": "Enter character trait"
                     }}
                 />
             </div>
@@ -136,7 +136,8 @@ class StartPage extends React.Component {
                     {this.state.isMatch === false
                         ?
                             <div>
-                                <p className="h2 text-center" style={{paddingTop: "100px", paddingBottom: "50px"}}>What do you value in a teammate?</p>
+                            <p className="h1 text-center" style={{ color: "#0C3A3C", paddingTop: "100px", paddingBottom: "40px" }}>Get Your Business Developer </p>
+                            <p className="h2 text-center" style={{ color: "#335D57", paddingTop: "0px", paddingBottom: "50px" }}>Select qualities of your ideal teammate. </p>
                                 <div className="container">
                                     <SearchBox />
                                     <div className="text-center mt-5">
@@ -146,7 +147,7 @@ class StartPage extends React.Component {
                                                 style={{borderRadius: "20px"}}
                                                 onClick={this.onFindMatchClick}
                                             >
-                                                    <p className="h4 p-2 my-auto">Find a match</p>
+                                                    <p className="h4 p-2 my-auto">FIND MATCH</p>
                                             </button>
                                     </div>
                                 </div>
@@ -156,7 +157,7 @@ class StartPage extends React.Component {
                                 {this.state.startLoadingQualities
                                     ?
                                         <div className="text-center" style={{paddingTop: "100px"}}>
-                                            <p className="h2 mb-5">Searching for the qualities selected...</p>
+                                            <p className="h2 mb-5">Matching character traits...</p>
                                             <GridLoader
                                                 css={override}
                                                 size={40}
@@ -170,7 +171,7 @@ class StartPage extends React.Component {
                                 {this.state.startLoadingTeammate
                                     ?
                                         <div className="text-center" style={{paddingTop: "100px"}}>
-                                            <p className="h2 mb-5">Searching for the ideal teammate...</p>
+                                            <p className="h2 mb-5">Searching ideal teammate...</p>
                                             <HashLoader
                                                 css={override}
                                                 size={80}
@@ -182,16 +183,16 @@ class StartPage extends React.Component {
                                         null
                                 }
                                 <Delayed waitBeforeShow={4000}>
-                                    <p className="h2 text-center" style={{paddingTop: "100px"}}>
-                                        It's a match!
-                                    </p>
-                                    <div className="text-center" style={{paddingBottom: "50px"}}>
-                                        <p className="h1 text-success mb-4">Leon Schulz</p>
+                                <p className="h2 text-center" style={{ fontStyle: "italic", color: "#0C3A3C", paddingTop: "100px", paddingBottom: "20px"}}>It's a Match!</p>
+                                <p className="h1 text-center" style={{ color: "#335D57", paddingBottom: "20px" }}>Leon Schulz </p>
+                                <div className="text-center" style={{ paddingBottom: "50px"}}>
                                         <img
                                             className="text-center"
                                             src="https://media-exp1.licdn.com/dms/image/C4D03AQGnBKoKlYTDnA/profile-displayphoto-shrink_800_800/0?e=1600300800&v=beta&t=LGqy545XrQcX1fV2Q-AAmfpu4dXkdWS_31B0VoZEyM8"
                                             style={{width: "300px", borderRadius: "150px"}}
                                         />
+                                    <p className="text-black" style={{ fontWeight: "bold", fontSize: 18, paddingTop: "30px", paddingBottom: "10px"}}>EXPERIENCE</p>
+                                    <p className="text-black" style={{ color: "black", fontSize: 18 }}>Evaluating investment opportunities for a deep tech VC has trained me in analyzing markets. <br /> <br /> Building B2B ventures for Europe's leading, independent company builder has taught me to understand customer needs. <br /><br /> Managing fundraising for a social project at Engineers Without Borders has schooled me in controlling finances. <br /><br /> With my programming skills (Python, Java, Matlab, R) I will support the Tech Developer. <br /><br /> With my hands-on experience in Design Thinking I will support the Problem Expert.</p>
                                         <div className="container border bg-white p-3 mt-5" style={{width: "500px"}}>
                                             <div className="col" style={{maxHeight:"300px", overflowY:"scroll"}}>
                                                 {globalSelectedList.map((item) =>
@@ -211,7 +212,7 @@ class StartPage extends React.Component {
                                                 style={{borderRadius: "20px"}}
                                                 onClick={() => {location.href = 'https://www.linkedin.com/in/leon-schulz/'}}
                                             >
-                                                <p className="h4 p-2 my-auto">Check out LinkedIn profile</p>
+                                                <p className="h4 p-2 my-auto">EXPLORE LINKEDIN</p>
                                             </button>
                                         </div>
                                         <div>
